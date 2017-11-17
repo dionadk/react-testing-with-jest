81cbc7020ebe7839ce4fc01797ed2ce86118b7a8
Diona Kurien <diona.david@gmail.com>
2017-11-17 10:42:25 -0500

    added jest configuration

---
+++ starter/CountdownTimer/app/_tests_/components/Clock.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Clock from 'Clock';
+
+describe('Clock', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDom.render(<Clock/>, div);
+  });
+});
